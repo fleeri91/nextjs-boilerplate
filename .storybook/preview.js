7ff80fcf83142fde1ceff10ce0d7b8a7443f4d70
Flemming Eriksson <flemming.eriksson@consid.se>
2023-02-28 08:08:46 +0100

    Hierarchy sorting

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,25 @@
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+  options: {
+    storySort: (a, b) =>
+      a[1].kind === b[1].kind
+        ? 0
+        : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
+    order: [
+      '1. Documentations',
+      ['1 - Introduction', '2 - Convenctions'],
+      '2. Theme',
+      ['Colors', 'Typography'],
+      '3. Components',
+      [('Atoms', 'Molecules', 'Organisms')],
+      '4. Layouts',
+      '5. Pages',
+    ],
+  },
+};
