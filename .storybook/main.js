4f5b6b57db46163a9421e154e9e9d7cebd4edede
Flemming <fleeri91@gmail.com>
2023-03-05 22:31:55 +0100

    postcss and ts path setup

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,4 @@
-const path = require('path');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 module.exports = {
   stories: [
@@ -11,20 +11,26 @@ module.exports = {
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
     '@storybook/addon-a11y',
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss')
+        }
+      }
+    }
   ],
   framework: '@storybook/react',
   core: {
     builder: '@storybook/builder-webpack5',
   },
   webpackFinal: (config) => {
-    config.resolve.alias = {
-      ...config.resolve?.alias,
-      '@': [path.resolve(__dirname, '../src/'), path.resolve(__dirname, '../')],
-    };
-    config.resolve.roots = [
-      path.resolve(__dirname, '../public'),
-      'node_modules',
+    config.resolve.plugins = [
+      ...(config.resolve.plugins || []),
+      new TsconfigPathsPlugin({
+        extensions: config.resolve.extensions
+      })
     ];
     return config;
-  },
+  } 
 };
